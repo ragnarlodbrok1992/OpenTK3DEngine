a36235a63a480d8b5e3268084727616f97ffbcf7
Maciej Ragnar Oliwa <maraus92@gmail.com>
2023-08-17 07:52:01 +0200

    Working Texture!

--- primitives/Texture.cs
+++ primitives/Texture.cs
@@ -16,15 +16,17 @@ namespace OpenTK3DEngine
       Handle = GL.GenTexture();
       this.filePath = filePath;
 
+      // Loading texture from OpenTK tutorial
+      GL.ActiveTexture(TextureUnit.Texture0);
+      GL.BindTexture(TextureTarget.Texture2D, Handle);
+
       // Some global texture setup
       GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int) TextureWrapMode.Repeat);
       GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int) TextureWrapMode.Repeat);
       GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
       GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
 
-      // Loading texture from OpenTK tutorial
-      GL.ActiveTexture(TextureUnit.Texture0);
-      GL.BindTexture(TextureTarget.Texture2D, Handle);
+
       StbImage.stbi_set_flip_vertically_on_load(1);
       ImageResult image = ImageResult.FromStream(File.OpenRead(this.filePath), ColorComponents.RedGreenBlueAlpha);
       GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
@@ -34,7 +36,7 @@ namespace OpenTK3DEngine
 
     public void Use()
     {
-      // GL.ActiveTexture(TextureUnit.Texture0);
+      GL.ActiveTexture(TextureUnit.Texture0);
       GL.BindTexture(TextureTarget.Texture2D, Handle);
     }
   }
